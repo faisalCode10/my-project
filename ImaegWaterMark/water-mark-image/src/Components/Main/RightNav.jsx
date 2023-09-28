8ffef99295a9cbc0e22415070dbbf17edd3e34cc
faisalCode10 <faisalimran477@gmail.com>
2023-09-28 03:42:23 -0700

    Some changes in EdtingImage UI

--- ImaegWaterMark/water-mark-image/src/Components/Main/RightNav.jsx
+++ ImaegWaterMark/water-mark-image/src/Components/Main/RightNav.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import {AiOutlinePicture} from 'react-icons/ai'
-import {BiText} from 'react-icons/bi'
+import { AiOutlinePicture } from 'react-icons/ai'
+import { BiText } from 'react-icons/bi'
 import './RightNav.css'
 const RightNav = () => {
   return (
     <div>
-    <div className="right-nav">
+      <div className="right-nav">
         <h1>Water Mark Images</h1>
         <div className="buttons">
-  <AiOutlinePicture style={{ fontSize: '32px' }} />
-  <button>ADD IMAGE</button>
-  <BiText style={{ fontSize: '32px' }} />
-  <button>ADD Text</button>
-</div>
+          <div className="btn-first">
+            <AiOutlinePicture className='icon' />
+            <button>ADD IMAGE</button>
+          </div>
+          <div className="btn-second">
+            <BiText className='icon' />
+            <button>ADD Text</button>
+          </div>
+        </div>
+
+          <div className="save-btn">
+            <button className='btn'><p>WaterMark Image</p></button>
+          </div>
 
       </div>
     </div>
